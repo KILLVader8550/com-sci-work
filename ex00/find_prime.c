0640a62b7c33d79cf431644da25ef2a225b72229
KILLVader8550 <145247735+KILLVader8550@users.noreply.github.com>
2025-06-16 11:45:54 +0700

    ex00

--- ex00/find_prime.c
+++ ex00/find_prime.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
+void get_num (int *nb)
+{
+    printf("Enter number: ");
+    scanf("%d", nb);
+}
+
+void is_prime (int nb)
 {
     int i;
-    int nb;
 
     i = 0;
-    printf("Enter number: ");
-    scanf("%d", &nb);
     while (i++ < sqrt(nb))
     {
         if (nb % i == 0)
         {    
             printf("Is not prime.\n");
-            return (0);
+            return ;
         }
     }
     printf("Is prime.\n");
+}
+
+int main()
+{
+    int i;
+    int nb1;
+    int nb2;
+
+    get_num(&nb1);
+    printf("Your number: %d | ", nb1);
+    is_prime(nb1);
+    get_num(&nb2);
+    printf("Your number divide by 2: %d | ", nb2 / 2);
+    is_prime(nb2);
     return (0);
 }
