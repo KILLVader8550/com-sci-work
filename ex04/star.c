3e2c070edc66b58a59b1b1b1570b1e2a8daa22da
KILLVader8550 <145247735+KILLVader8550@users.noreply.github.com>
2025-07-03 14:35:29 +0700

    ex04

--- ex04/star.c
+++ ex04/star.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-int main()
+void    ft_star()
 {
     for (char i = '0'; i < ':'; i++)
     {
@@ -8,5 +8,53 @@ int main()
             write(1, "*", 1);
         write(1, "\n", 1);
     }
+}
+
+void    ft_star_reverse()
+{
+    for (char i = '0'; i < ':'; i++)
+    {
+        for (char j = ':'; j > i; j--)
+            write(1, "*", 1);
+        write(1, "\n", 1);
+    }
+}
+
+void    ft_star_back()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10 - i - 1; j++)
+            write(1, " ", 1);
+        for (int k = 0; k <= i; k++)
+            write(1, "*", 1);
+
+        write(1, "\n", 1);
+    }
+}
+
+void    ft_star_back_reverse()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < i; j++)
+            write(1, " ", 1);
+        for (int k = 0; k < 10 - i; k++)
+            write(1, "*", 1);
+
+        write(1, "\n", 1);
+    }
+}
+
+int main()
+{
+    ft_star();
+    write(1, "\n", 1);
+    ft_star_reverse();
+    write(1, "\n", 1);
+    ft_star_back();
+    write(1, "\n", 1);
+    ft_star_back_reverse();
+    write(1, "\n", 1);
     return (0);
 }
